366ea87da802d5a2251e475733776029bdfdc90c
regretyou710 <regretyou710@gmil.com>
2022-06-20 20:11:45 +0800

    202206202011

---
+++ Demos/Demo10_創建MyBatis的映射文件/src/main/java/tw/com/mybatis/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package tw.com.mybatis.mapper;
+
+public interface UserMapper {
+
+    /**
+     * MyBatis面向介面編程的兩個一致:
+     * 1. 映射文件的namespace要和mapper介面的全類名保持一致
+     * 2. 映射文件中SQL語句的id要和mapper介面中的方法名一致
+     *
+     * 表--實體類--mapper介面--映射文件
+     */
+
+    /**
+     * 添加用戶訊息
+     */
+    int insertUser();
+}
