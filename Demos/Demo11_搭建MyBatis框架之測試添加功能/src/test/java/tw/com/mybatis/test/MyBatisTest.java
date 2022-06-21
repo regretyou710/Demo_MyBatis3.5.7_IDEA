e0fef3d5c1c1e0a0403bb4136a142deffc46ef37
regretyou710 <regretyou710@gmil.com>
2022-06-21 21:48:24 +0800

    202206212148

---
+++ Demos/Demo11_搭建MyBatis框架之測試添加功能/src/test/java/tw/com/mybatis/test/MyBatisTest.java
@@ -0,0 +1,40 @@
+package tw.com.mybatis.test;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import tw.com.mybatis.mapper.UserMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+
+    @Test
+    public void testMyBatis() throws IOException {
+        //加載核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+        //獲取SqlSessionFactoryBulider
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+
+        //獲取SqlSessionFactory
+        SqlSessionFactory SqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+
+        //獲取SqlSession(代表Java程序和數據庫之間的會話)
+        SqlSession sqlSession = SqlSessionFactory.openSession();
+
+        //獲取mapper介面對象(底層透過代理模式獲取)
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //測試方法
+        int result = mapper.insertUser();
+
+        //因為核心配置文件中交易事務管理器是設定JDBC(使用原始的JDBC管理事務，所有事務的提交或回滾都要手動處理)
+        sqlSession.commit();
+
+        System.out.println(result);
+    }
+}
